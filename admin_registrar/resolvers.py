889f3e46635292d13bd103715a3ab335791a9970
VideoKompany <135313844+VlaDOS1408@users.noreply.github.com>
2026-07-29 00:50:52 +0400

    FIX: use updated settings.ADMINS_FOR_MODELS in resolvers

--- admin_registrar/resolvers.py
+++ admin_registrar/resolvers.py
@@ -1,33 +1,18 @@
 from typing import Protocol
 
-from django.utils.module_loading import import_string
 from django.contrib.admin import ModelAdmin
 from django.db.models import Model
 
-# Circular import
-# from admin_registrar.conf import settings
+from admin_registrar.conf import settings
 
 
 class DefaultAdminsResolver(Protocol):
 	def __call__(self, model_class: type[Model]) -> type[ModelAdmin]: ...
 
-# caching
-_PARSED_DEFAULTS: dict[type[Model], type[ModelAdmin]] = {}
 def first_mro_match_resolver(model_class: type[Model]) -> type[ModelAdmin]:
-	from admin_registrar.conf import settings
-	global _PARSED_DEFAULTS
-
-	if not _PARSED_DEFAULTS:
-		_PARSED_DEFAULTS = {
-			import_string(model_path): import_string(admin_path)
-			for model_path, admin_path in
-			settings.ADMINS_FOR_MODELS.items()
-		}
-
 	for cls in model_class.mro():
 		if not issubclass(cls, Model):
 			continue # If model nesting from some mixins who not nested from Model
-		if cls in _PARSED_DEFAULTS:
-			return _PARSED_DEFAULTS[cls]
-
+		if cls in settings.ADMINS_FOR_MODELS:
+			return settings.ADMINS_FOR_MODELS[cls]
 	return ModelAdmin
