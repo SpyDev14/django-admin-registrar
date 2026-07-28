2e0c835735e3ab9f455f451367438e33ecaa15ed
VideoKompany <135313844+VlaDOS1408@users.noreply.github.com>
2026-07-29 00:42:13 +0400

    rename RECCOMENDED_* formatter and name in logging to RECOMMENDED_*

--- admin_registrar/logging.py
+++ admin_registrar/logging.py
@@ -1,10 +1,8 @@
 from admin_registrar.utils.colors import Fore
 
 
-RECCOMENDED_FORMATTER = {
+RECOMMENDED_FORMATTER = {
 	'format': '[{levelname}] %sAdmin registrar%s: {message}' % (Fore.CYAN, Fore.RESET),
 	'style': '{',
-},
-
-# TODO: Добавить форматтер или обработчик, вырезающий ANSI коды по этому регексу:
-# re.compile('\001?\033\\[((?:\\d|;)*)([a-zA-Z])\002?')
+}
+RECOMMENDED_NAME = 'admin_registrar.registrars'
