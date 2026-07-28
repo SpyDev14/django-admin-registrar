f52122200060e22c3af10689572483c7bb43a74e
SpyDev14 <135313844+SpyDev14@users.noreply.github.com>
2026-07-28 17:20:54 +0400

    rework conf.py

--- admin_registrar/conf.py
+++ admin_registrar/conf.py
@@ -1,27 +1,33 @@
+from dataclasses import is_dataclass
 from functools import cached_property
-from typing import Generic, TypeVar, Self, overload, Any
+from typing import Generic, TypeVar, Self, overload, Any, TYPE_CHECKING
+import logging
 
 from django.utils.module_loading import import_string
 from django.contrib.admin import ModelAdmin
+from django.db.models import Model
 from django.conf import settings as django_settings
 
-from admin_registrar.resolvers import DefaultAdminsResolver, first_mro_match_resolver
 from admin_registrar.utils import typename
-from admin_registrar.admin import HiddenAdmin
+
+if TYPE_CHECKING:
+	from admin_registrar.dataclasses import RegisteringLogColors
+	from admin_registrar.resolvers import DefaultAdminsResolver
+
 
 _CONFIG_DICT_NAME = 'ADMIN_REGISTRAR'
-_ADMIN_REGISTRATION_CONFIG: dict[str, Any] = getattr(django_settings, _CONFIG_DICT_NAME, {})
-if not isinstance(_ADMIN_REGISTRATION_CONFIG, dict):
-	raise TypeError(f'{_CONFIG_DICT_NAME} should be a dict, got: {typename(_ADMIN_REGISTRATION_CONFIG)}.')
+_CONFIG_DICT: dict[str, Any] = getattr(django_settings, _CONFIG_DICT_NAME, {})
+if not isinstance(_CONFIG_DICT, dict):
+	raise TypeError(f'{_CONFIG_DICT_NAME} should be a dict, got: {typename(_CONFIG_DICT)}.')
 
 _T = TypeVar('_T')
 # Generic for default type hints (if not specified)
-class ConfValue(Generic[_T]):
+class ConfVar(Generic[_T]):
 	def __set_name__(self, owner: type, name: str):
 		self._name = name
 
-	def __init__(self, default: _T):
-		self._default = default
+	def __init__(self, raw_default: Any):
+		self._raw_default = raw_default
 
 	def __str__(self):
 		return self._name
@@ -31,7 +37,7 @@ class ConfValue(Generic[_T]):
 		return self._get_value()
 
 	def _get_value(self):
-		return _ADMIN_REGISTRATION_CONFIG.get(self._name, self._default)
+		return _CONFIG_DICT.get(self._name, self._raw_default)
 
 	@overload
 	def __get__(self, instance: None, owner: type) -> Self: ...
@@ -42,23 +48,44 @@ class ConfValue(Generic[_T]):
 			return self
 		return self._value
 
-class ConfImportableVal(ConfValue[_T]):
-	def _get_value(self):
-		specified_path = _ADMIN_REGISTRATION_CONFIG.get(self._name)
-		if specified_path is not None and not isinstance(specified_path, str):
-			raise TypeError(f"Importable {self._name} shoud be a string")
-		return (
-			import_string(specified_path) if specified_path
-			else self._default
-		)
+class ImportableConfVar(ConfVar[_T]):
+	def __init__(self, raw_default: str):
+		super().__init__(raw_default)
+		self._raw_default: str
 
-# Settings can be refactored to `Config` with `base` property and
+	def _get_value(self):
+		import_str = _CONFIG_DICT.get(self._name)
+		if import_str is not None and not isinstance(import_str, str):
+			raise TypeError(f"Importable {self._name} shoud be a string")
+
+		return import_string(import_str or self._raw_default)
+
+class ImportablesDictConfVar(ConfVar[_T]):
+	def __init__(self, raw_default: dict[str, str]):
+		super().__init__(raw_default)
+		self._raw_default: dict[str, str]
+
+	def _get_value(self):
+		data: dict[str, str] = _CONFIG_DICT.get(self._name, self._raw_default)
+		if not isinstance(data, dict):
+			raise TypeError(f"{self._name} should be a dict, got: {typename(data)}")
+
+		return {
+			import_string(key): import_string(value)
+			for key, value in data.items()
+		}
+
+# Settings can be refactored to `Config` with `base` and
 # values-nesting for global defaults and local overrides
 class Settings:
-	# DEFAULT_ADMINS_RESOLVER can be refactored!!!
-	DEFAULT_ADMINS_RESOLVER: ConfImportableVal[DefaultAdminsResolver] = ConfImportableVal(first_mro_match_resolver)
-	HIDDEN_ADMIN_CLASS:      ConfImportableVal[type[ModelAdmin]]      = ConfImportableVal(HiddenAdmin)
-	ADMINS_FOR_MODELS = ConfValue({})
-	COLORED_LOGS      = ConfValue(False)
+	HIDDEN_ADMIN_CLASS: ImportableConfVar[type[ModelAdmin]] = ImportableConfVar('admin_registrar.admin.HiddenAdmin')
+	# ADMINS_FOR_MODELS used only in admin_registrar resolvers by directly get from settings
+	# Maybe should refactor it for future allowing custom rules?
+	# Like "core.models.base.BaseRenderableModel+mptt.models.MPTTModel": "core.admin.base.RenderableMPTTAdmin"
+	# what catch models who mro is [BaseRenderableModel, MPTTModel, ...]
+	ADMINS_FOR_MODELS: ImportablesDictConfVar[dict[type[Model], type[ModelAdmin]]] = ImportablesDictConfVar({})
+	ENABLE_COLORED_LOGS: ConfVar[bool] = ConfVar(True)
+
+	DEFAULT_ADMINS_RESOLVER: ImportableConfVar["DefaultAdminsResolver"] = ImportableConfVar('admin_registrar.resolvers.first_mro_match_resolver')
 
 settings = Settings()
