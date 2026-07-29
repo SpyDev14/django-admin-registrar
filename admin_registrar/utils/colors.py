46361b69d7c03ee99cdda41c38d21a237966b47a
SpyDev14 <135313844+SpyDev14@users.noreply.github.com>
2026-07-29 13:38:10 +0400

    replace global import settings to utils.colors and global if to some construction wit lazy resolving by metaclass

--- admin_registrar/utils/colors.py
+++ admin_registrar/utils/colors.py
@@ -1,4 +1,5 @@
-from admin_registrar.conf import settings
+# CI
+# from admin_registrar.conf import settings
 
 class _Palette:
 	BLACK: 	str = ''
@@ -42,12 +43,35 @@ class _ForePalette(_Palette):
 
 	RESET: 	str = '\033[39m'
 
-Fore: type[_Palette] = _Palette
+# TODO: Write normal comment
+# I just wanted Fore marked green as class, not ligt-blue as var.
+# It is one reason why i use classes dirrectly and metaclass for resolving.
+# But i don't thing what metaclass and class as object is really bad in
+# comparing with class and instance. Come one, class is regular object!
+# And metaclass is regular class what just making other classes.
 
-if settings.COLORED_LOGS:
-	# If user use cmd.exe they install colorama
-	try: from colorama import init; init()
-	except ImportError:
-		pass
+# Main reason why i use metaclass instead instance of regular class (what similar
+# overwrite self to _Palette or _ForePalette) is avoid from `# type: ignore`. Yeap.
 
-	Fore = _ForePalette
+# Sometimes ago choosing class (empty or with ansi codes) was in `if` in
+# global space (code in __getattribute__ was in global space), but it
+# start leads to Circular Import
+class _UnsolvedForeMeta(type):
+	def __getattribute__(self, name):
+		from admin_registrar.conf import settings
+		global Fore
+
+		Fore = _Palette
+		if settings.ENABLE_COLORED_LOGS:
+			try: from colorama import init; init()
+			except ImportError:
+				pass
+			Fore = _ForePalette
+		return getattr(Fore, name)
+
+# Equals to _UnsolvedFore = _UnsolvedForeMeta("_UnsolvedFore", (_PlugPalette,), {})
+class _UnsolvedFore(_Palette, metaclass=_UnsolvedForeMeta):
+	pass
+
+
+Fore: type[_Palette] = _UnsolvedFore
